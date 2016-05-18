05587af08037b47b85aacc2806b6113bfba1c25f
Paul Yakshankin <yakspavel@gmail.com>
2016-05-18 23:55:59 +0400

    fixed matcher

--- lib/api_doc_generator/rspec/matchers/match_response_schema.rb
+++ lib/api_doc_generator/rspec/matchers/match_response_schema.rb
@@ -1,9 +1,8 @@
 RSpec::Matchers.define :match_response_schema do |schema|
   match do |response|
-    # path = Rails.root.join("schemas", schema + ".json")
-    # @errors = JSON::Validator.fully_validate(path.to_s, response.body)
-    # @errors.blank?
-    true
+    path = Rails.root.join("schemas", schema + ".json")
+    @errors = JSON::Validator.fully_validate(path.to_s, response.body)
+    @errors.blank?
   end
 
   failure_message do
